cbf160ac06d61a161eb1bb6a82a687179eaa25d6
Berlian Safri Prakoso <berliansafrip@gmail.com>
2021-03-17 23:26:25 +0700

    add exc 3.2

---
+++ Exercise 3.2.py
@@ -0,0 +1,9 @@
+list = ["fisika", "kimia", 2019, 1945]
+list.append(2001)
+list.insert(2, 2077)
+print("nilai pada index ke-2", list[2])
+print(list)
+
+
+list[1] = "Kalkulus"
+print("index ke-2 setelah di update:", list[1])

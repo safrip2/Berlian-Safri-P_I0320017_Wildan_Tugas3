ed139361502b032bf6275701717585d62fcde466
Berlian Safri Prakoso <berliansafrip@gmail.com>
2021-03-18 15:16:45 +0700

    add tugas 1

---
+++ tugas 1.py
@@ -0,0 +1,12 @@
+friends = ["Habib", "Fahri", "Jimly", "Andi", "Amel", "Amanda", "Diondi", "Andrew", "Samuel", "Nanang"]
+friends[2] = "Diki"
+friends[4] = "Ameng"
+friends[8] = "Kristiono"
+print("index 4,6,7 is", friends[4], friends[6], friends[7])
+print("Jimly A.K.A", friends[2], ", Amel A.K.A", friends[4], ", Samuel A.K.A", friends[8])
+friends.append("Dini")
+friends.insert(10, "Savira")
+print(friends)
+for friend in friends:
+    print(friend)
+print("total item in this list:", len(friends))
